c1c94014488cbacf8e2eaaff04d887d78bd0cc52
rominadouk <douk.romina@gmail.com>
2023-11-04 20:31:56 -0700

    commented out specified port

--- controllers/server.js
+++ controllers/server.js
@@ -236,6 +236,6 @@ mongoose.connection.once('open', () => {
 });
 
 
-app.listen(4000, () => {
-    console.log('listening...')
-});
+// app.listen(4000, () => {
+//     console.log('listening...')
+// });
